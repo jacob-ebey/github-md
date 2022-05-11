fb246ef32bf0bc47e6a2391251bfca038fa6c457
Jacob Ebey <jacob.ebey@live.com>
2022-05-11 02:18:37 -0700

    updated so demo works in deployment

--- src/index.ts
+++ src/index.ts
@@ -11,96 +11,109 @@ type Env = {
   GITHUB_MD: KVNamespace;
 };
 
-export default {
+const entry = {
   async fetch(
     request: Request,
     { GITHUB_MD }: Env,
     ctx: ExecutionContext
   ): Promise<Response> {
-    let url = new URL(request.url);
-    let domain = new URL("/", url).href;
+    try {
+      let url = new URL(request.url);
+      let domain = new URL("/", url).href;
 
-    if (url.pathname === "/") {
-      let html = renderToString(createElement(Docs, { domain }));
-      return new Response("<!DOCTYPE html>" + html, {
-        headers: { "Content-Type": "text/html" },
+      if (url.pathname === "/") {
+        let html = renderToString(createElement(Docs, { domain }));
+        return new Response("<!DOCTYPE html>" + html, {
+          headers: { "Content-Type": "text/html" },
+        });
+      }
+
+      if (url.pathname.startsWith("/_demo/")) {
+        let file = url.pathname.slice("/_demo".length);
+        console.log({ href: new URL(file, domain).href });
+        let markdownResponse = await entry.fetch(
+          new Request(new URL(file, domain).href),
+          { GITHUB_MD },
+          ctx
+        );
+        let markdown = (await markdownResponse.json()) as {
+          error: string;
+          html: string;
+        };
+        let html = renderToString(
+          createElement(Demo, { html: markdown.html || markdown.error })
+        );
+        return new Response("<!DOCTYPE html>" + html, {
+          status: markdownResponse.status,
+          headers: { "Content-Type": "text/html" },
+        });
+      }
+
+      let kvJsonKey = `json${url.pathname}`;
+      let json = await GITHUB_MD.get(kvJsonKey);
+
+      if (json) {
+        return new Response(json, {
+          headers: { "Content-Type": "application/json" },
+        });
+      }
+
+      let kvMarkdownKey = `md${url.pathname}`;
+      let markdown = await GITHUB_MD.get(kvMarkdownKey);
+
+      if (!markdown) {
+        let contentResponse = await fetch(
+          new URL(url.pathname, "https://raw.githubusercontent.com/").href
+        );
+        if (contentResponse.ok) {
+          markdown = await contentResponse.text();
+          ctx.waitUntil(
+            GITHUB_MD.put(kvMarkdownKey, markdown, { expirationTtl: 10 * 60 })
+          );
+        }
+      }
+
+      if (!markdown) {
+        return new Response(JSON.stringify({ error: "Not found" }), {
+          status: 404,
+          headers: { "Content-Type": "application/json" },
+        });
+      }
+
+      let parser = new MarkdownParser({
+        html: true,
+        linkify: true,
+        langPrefix: "hljs language-",
+        highlight: (str, lang) => {
+          if (lang && hljs.getLanguage(lang)) {
+            try {
+              return hljs.highlight(str, { language: lang }).value;
+            } catch (__) {}
+          }
+
+          return ""; // use external default escaping
+        },
       });
-    }
 
-    if (url.pathname.startsWith("/_demo/")) {
-      let file = url.pathname.slice("/_demo".length);
-      let markdownResponse = await fetch(new URL(file, domain).href);
-      let markdown = (await markdownResponse.json()) as {
-        error: string;
-        html: string;
-      };
-      let html = renderToString(
-        createElement(Demo, { html: markdown.html || markdown.error })
-      );
-      return new Response("<!DOCTYPE html>" + html, {
-        status: markdownResponse.status,
-        headers: { "Content-Type": "text/html" },
+      let { body, attributes } = frontmatter(markdown);
+      let html = parser.render(body);
+
+      json = JSON.stringify({
+        attributes,
+        html,
       });
-    }
 
-    let kvJsonKey = `json${url.pathname}`;
-    let json = await GITHUB_MD.get(kvJsonKey);
+      ctx.waitUntil(GITHUB_MD.put(kvJsonKey, json, { expirationTtl: 10 * 60 }));
 
-    if (json) {
       return new Response(json, {
         headers: { "Content-Type": "application/json" },
       });
+    } catch (error: any) {
+      console.error(error?.message || error);
+      error?.stack && console.error(error.stack);
+      return new Response("Something went wrong", { status: 500 });
     }
-
-    let kvMarkdownKey = `md${url.pathname}`;
-    let markdown = await GITHUB_MD.get(kvMarkdownKey);
-
-    if (!markdown) {
-      let contentResponse = await fetch(
-        new URL(url.pathname, "https://raw.githubusercontent.com/").href
-      );
-      if (contentResponse.ok) {
-        markdown = await contentResponse.text();
-        ctx.waitUntil(
-          GITHUB_MD.put(kvMarkdownKey, markdown, { expirationTtl: 10 * 60 })
-        );
-      }
-    }
-
-    if (!markdown) {
-      return new Response(JSON.stringify({ error: "Not found" }), {
-        status: 404,
-        headers: { "Content-Type": "application/json" },
-      });
-    }
-
-    let parser = new MarkdownParser({
-      html: true,
-      linkify: true,
-      langPrefix: "hljs language-",
-      highlight: (str, lang) => {
-        if (lang && hljs.getLanguage(lang)) {
-          try {
-            return hljs.highlight(str, { language: lang }).value;
-          } catch (__) {}
-        }
-
-        return ""; // use external default escaping
-      },
-    });
-
-    let { body, attributes } = frontmatter(markdown);
-    let html = parser.render(body);
-
-    json = JSON.stringify({
-      attributes,
-      html,
-    });
-
-    ctx.waitUntil(GITHUB_MD.put(kvJsonKey, json, { expirationTtl: 10 * 60 }));
-
-    return new Response(json, {
-      headers: { "Content-Type": "application/json" },
-    });
   },
 };
+
+export default entry;
